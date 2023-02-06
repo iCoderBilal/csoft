088b15ba8fd5aeed9ff52c7a2d8739e04d487bbc
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-02-06 12:35:11 +0500

    Starting new section

---
+++ src/HomepageSection/Partners/Partners.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './assets/partners.css'
+
+const Partners = () => {
+  return (
+    <>
+    <div className="partners-section" data-scroll-section>
+        <div className="partners-container">
+            <div className="partners-heading">
+                <h2>Our Partners</h2>
+            </div>
+           <div className="partners-logo-cards">
+            
+            <div className="partners-logo">
+                
+            </div>
+
+           </div>
+        </div>
+    </div>
+    
+    </>
+  )
+}
+
+export default Partners
