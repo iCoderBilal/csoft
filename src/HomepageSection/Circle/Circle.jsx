eb11f5fa00e662ce2e5241e2b7a1ea8fb3906aac
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-02-06 09:41:47 +0500

    designing Circle section

--- src/HomepageSection/Circle/Circle.jsx
+++ src/HomepageSection/Circle/Circle.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './circle.css'
+import ourServicesImg from './assets/our-services-img.svg'
 
 const Circle = () => {
   return (
@@ -8,6 +9,13 @@ const Circle = () => {
       <div className="circle-container">
         <div className="circle-heading">
             <h2><span className='what-we-spell'>What We Do</span> <br/><span className='h-blue'>Full</span> Range of <span className='h-blue'>Services</span> Your Company Desires</h2>
+        </div>
+        <div className="circle-main-content">
+            <div className="left"></div>
+            <div className="center">
+                <img src={ourServicesImg} alt="" />
+            </div>
+            <div className="right"></div>
         </div>
       </div>
     </section>
