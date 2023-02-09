9348abbdf4d4b7aae326a97edd27fbfc937e5276
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-02-09 20:27:53 +0500

    completed

--- src/HomepageSection/Projects/Projects.jsx
+++ src/HomepageSection/Projects/Projects.jsx
@@ -11,8 +11,8 @@ const Projects = () => {
     <section id="projects-section" data-scroll-section>
      <div className="projects-container" id='projects-container'>
         <div className="projects-heading" id='projects-heading'>
-            <h2 data-scroll data-scroll-direction="horizontal" data-scroll-speed="-25" data-scroll-target="#projects-heading" className='recent-spell'>RECENT</h2>
-            <h2 className='projects-spell' data-scroll data-scroll-direction="horizontal" data-scroll-speed="25" data-scroll-target="#projects-heading">PROJECTS</h2>
+            <h2 data-scroll data-scroll-direction="horizontal" data-scroll-speed="-15" data-scroll-target="#projects-heading" className='recent-spell'>RECENT</h2>
+            <h2 className='projects-spell' data-scroll data-scroll-direction="horizontal" data-scroll-speed="15" data-scroll-target="#projects-heading">PROJECTS</h2>
         </div>
 
 
@@ -29,13 +29,35 @@ const Projects = () => {
                 </div>
                 <div className="box">
                     <img src={cisco}/>
+                    <div className='box-info-name'>
+                  <h4>Netacad</h4>
+                  </div>
+                  <div className="box-info-button">
+                  <button>View</button>
+                </div>
                 </div>
                 <div className="box">
                     <img src={raraGloves}/>
+                    <div className='box-info-name'>
+                  <h4>Rara Gloves</h4>
+                  </div>
+                  <div className="box-info-button">
+                  <button>View</button>
+                </div>
                 </div>
                 <div className="box">
                 <img src={darco}/>
+                <div className='box-info-name'>
+                  <h4>Darco</h4>
+                  </div>
+                  <div className="box-info-button">
+                  <button>View</button>
                 </div>
+                </div>
+                </div>
+
+                <div className="more-projects-btn">
+                    <button>More Projects</button>
                 </div>
      </div>
     </section>
